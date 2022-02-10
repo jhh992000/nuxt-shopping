4939a70fb42d4dff3ee1ceb38e0c53a7baad1e1e
Hoonhee Jung <jhh992000@gmail.com>
2022-02-10 14:33:53 +0900

    feat: baseURL 오타 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,4 +39,12 @@ export default {
   server: {
     port: process.env.NODE_ENV === 'production' ? null : 4000,
   },
+
+  // env config
+  env: {
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://my-json-server.typicode.com/jhh992000/nuxt-shopping-api'
+        : 'http://localhost:3000',
+  },
 }
